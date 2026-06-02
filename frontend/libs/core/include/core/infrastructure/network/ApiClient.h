486501853818bfcef218dcae14c728e7bf58a6a7
streetms <konstantin.isakov.2003@mail.ru>
2026-06-02 19:25:56 +0700

    Api client теперь будет общаться с сервером через порт 443

--- frontend/libs/core/include/core/infrastructure/network/ApiClient.h
+++ frontend/libs/core/include/core/infrastructure/network/ApiClient.h
@@ -8,7 +8,7 @@ class ApiClient : public QObject
     Q_OBJECT
 public:
     explicit ApiClient(QObject *parent = nullptr);
-    static ApiClient* instance();
+    // static ApiClient* instance();
     void post(const QString &type, nlohmann::json data,std::function<void(QNetworkReply*)> callback);
 private:
     QNetworkAccessManager manager;
